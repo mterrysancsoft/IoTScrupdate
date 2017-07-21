970c04a9a2f89e4ee177b68f3c66c06a6b1971c9
Michael Terry <mterry@sancsoft.com>
2017-07-21 00:05:43 -0400

    switched over to environment variables

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Send a scrum update (scrupdate) message with an AWS IoT button.
 2. Update the role with support with SES permissions by adding actions for:
   - "ses:SendEmail"
   - "ses:SendRawEmail"
-3. Update the code with the contents of lambda.js
-  - Configure the region
-  - Configure the recipient list
-  - Configure the source address
+3. Define the Environment Variables for the Lambda function
+  - sourceEmail: from address; must be SES approved; ex: info@email.com
+  - sesRegion: AWS region for SES; ex: us-east-1
+  - recipientList: to addresses; ex: notify@email.com
 4. Click the button
--- lambda.js
+++ lambda.js
@@ -1,12 +1,11 @@
 // Lambda function to send a static email preconfigured with a boilerplate status update
-// Configure with ToAddresses: 
-// 1: set your AWS regaion
-// 2: configure the recipient list
-// 3: sending address - must be verified for sending with SES
+// Configure with environment variables
+// sesRegion: SES region ex: us-east-1
+// sourceEmail: from email address - must be SES verified
+// recipientList: to email adresseses
 var aws = require('aws-sdk');
-// 1: set AWS region
 var ses = new aws.SES({
-   region: 'us-east-1'
+   region: process.env.sesRegion
 });
 
 exports.handler = function(event, context) {
@@ -15,21 +14,19 @@ exports.handler = function(event, context) {
 
     var eParams = {
         Destination: {
-            // 2 - configure the recipient list
-            ToAddresses: ["your recipient list"]
+            ToAddresses: [process.env.recipientList]
         },
         Message: {
             Body: {
                 Text: {
-                    Data: "Running late. Be in soon.\n\nYesterday:\nMeetings\nMoving cards\nSorting email\nDealing with server issues\n\nToday:\nDoing what was supposed to be done yesterday\n\nIn My Way:\nRunning late\n"
+                    Data: "Running late. Be in soon.\n\nYesterday:\nMeetings\nMoving cards\nSorting email\nDealing with server issues\n\nToday:\nDoing what was supposed to be done yesterday\n"
                 }
             },
             Subject: {
                 Data: "Scrupdate for " + d.toLocaleDateString()
             }
         },
-        // 3: sending address - must be verified for sending with SES
-        Source: "your verified SES email address"
+        Source: process.env.sourceEmail
     };
 
     console.log('===SENDING EMAIL===');
@@ -38,9 +35,12 @@ exports.handler = function(event, context) {
         else {
             console.log("===EMAIL SENT===");
             console.log(data);
+
+
             console.log("EMAIL CODE END");
             console.log('EMAIL: ', email);
             context.succeed(event);
+
         }
     });
 
